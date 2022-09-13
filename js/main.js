63e40e17e5a5e06d46ea4e6a39467ecd4ab28a66
Martin6175903 <martin6175903@yandex.ru>
2022-09-13 09:09:24 +0300

    Creating a format JSON

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+let childs = document.querySelector('.authorization__inner').children;
 
 document.querySelector('.form__title--active-btn-1').onclick = function(event) {
-    let childs = document.querySelector('.authorization__form').children;
 
-    childs[1].classList.remove('form__box--hidden');
-    childs[0].classList.add('form__box--hidden');
+    childs[3].children[0].classList.remove('form__box--hidden');
+    childs[2].children[0].classList.add('form__box--hidden');
+
 };
 
 document.querySelector('.form__title--active-btn-2').onclick = function(event) {
-    let childs = document.querySelector('.authorization__form').children;
 
-    childs[0].classList.remove('form__box--hidden');
-    childs[1].classList.add('form__box--hidden');
+    childs[2].children[0].classList.remove('form__box--hidden');
+    childs[3].children[0].classList.add('form__box--hidden');
+
 };
+
+
+
+
 
 
 
--- js/registration-valid.js
+++ js/registration-valid.js
@@ -1,1 +1,24 @@
+// Создаем переменные для проверки двух паролей
 
+let passwordReg;
+let passwordConfirm;
+
+// Проверка валидности паролей.
+// 
+
+document.querySelector('.form__box-2').onchange = function(event) {
+    let target = event.target;
+
+    if (target.type == 'password') {
+        if (target.classList.contains('form__password')) passwordReg = target.value;
+        if (target.classList.contains('form__confirm-psw')) passwordConfirm = target.value;
+
+        if (passwordReg && passwordConfirm) {
+            if (passwordReg !== passwordConfirm) {
+                document.querySelector('.form__confirm-psw').setCustomValidity("Пароли не совпадают!");
+                console.log("Пароли не совпадают!");
+            } else document.querySelector('.form__confirm-psw').setCustomValidity("");
+        }
+    } else return;
+
+}
